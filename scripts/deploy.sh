098bb68ce321b51ce77fa9a9bcf333a68ad35f3d
Arik Maor <arikmaor@gmail.com>
2018-04-02 13:28:30 +0300

    Update deploy script to use force push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,8 @@ git reset HEAD
 git checkout -b deploy_to_heroku
 git add certs -f
 git commit -m "Add cert files for deployment"
-git push heroku master
-git reset HEAD^
+git push heroku deploy_to_heroku:master --force
 git checkout master
+git checkout deploy_to_heroku -- certs
+git reset HEAD
 git branch -D deploy_to_heroku
