966d5c96cc02d1506d4aedab5a603459d3625933
Arik Maor <arikmaor@gmail.com>
2018-04-12 17:46:13 +0300

    Typo fix

--- src/redis.js
+++ src/redis.js
@@ -7,7 +7,7 @@ var redisUrl = Url.parse(config.redisUrl);
 const client = redis.createClient(redisUrl.port, redisUrl.hostname)
 
 client.on("error", function (err) {
-  console.error("Error is redis client: " + err)
+  console.error("Error in redis client: " + err)
 });
 
 const COMMANDS = ['set', 'get', 'expire']
