bc45faa2eb6226c56f955bb016a8ba340e688e83
Arik Maor <arikmaor@gmail.com>
2018-05-24 18:31:35 +0300

    Remove deploy script

--- scripts/deploy.sh
+++
@@ -1,9 +0,0 @@
-git reset HEAD
-git checkout -b deploy_to_heroku
-git add certs -f
-git commit -m "Add cert files for deployment"
-git push heroku deploy_to_heroku:master --force
-git checkout master
-git checkout deploy_to_heroku -- certs
-git reset HEAD
-git branch -D deploy_to_heroku
