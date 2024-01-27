82b804c8d3a8438829fcf58f07fa43af6b9f4227
iceyShardZ <ynonn1000@gmail.com>
2024-01-27 22:22:32 +0200

    Updated the logout function

--- src/index.js
+++ src/index.js
@@ -122,7 +122,8 @@ async function init() {
 
   app.get('/logout',
     (req, res) => {
-      res.redirect(`${config.logoutUrl}?logoutURL=${config.logoutRedirectUrl}`)
+      let referer = req.get('Referer') != undefined ? req.get('Referer') : (!!req.query.rf != undefined && req.query.rf == 'space') ? 'https://space.uingame.co.il/' : 'https://www.uingame.co.il/' ;
+      res.redirect(`${config.logoutUrl}?logoutURL=${referer}`)
     }
   )
 
